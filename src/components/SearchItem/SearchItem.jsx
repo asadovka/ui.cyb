d9f44c119ce0648a0ad636bba41cc903f74676e5
Valentin Stavetski <vstavetski@gmail.com>
2019-02-17 22:59:39 +0700

    update search item

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import styles from './SearchItem.css';
-// import cx from 'classnames';
 
-export const SearchItem = ({ children, ...props }) => (
-    <div { ...props } key={ props.hash } className={ styles.searchItem }>
-        <a href={ `cyb://${children}` }>
-            {children}
-        </a>
-        <span>{props.rank}</span>
+export const SearchItem = ({
+    rank, disabled, children, ...props
+}) => (
+    <div { ...props } className={ styles.searchItem }>
+        {disabled ? (
+            children
+        ) : (
+            <a href={ `cyb://${children}` }>
+                { children }
+            </a>
+        )}
+        <span>{rank}</span>
     </div>
 );
